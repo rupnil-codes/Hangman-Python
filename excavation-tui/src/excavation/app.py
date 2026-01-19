6889b130eefd8781d648d0b93a1d6343ed6ce2d9
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-01-19 17:49:13 +0530

    v0-alpha.3
    Changed license
    Added requirements.txt
    And overall Nicely organized the file structure!

---
+++ excavation-tui/src/excavation/app.py
@@ -0,0 +1,20 @@
+from textual.app import App
+from textual import events
+
+class Excavation(App):
+    CSS_PATH = "css\\excavation.css"
+
+    def on_key(self, event: events.Key) -> None:
+        if event.key == "escape":
+            self.app.exit()  # close on esc
+
+    # def on_mount(self) -> None:
+        # self.push_screen(IntroScreen())
+
+
+if __name__ == "__main__":
+
+    app = Excavation()
+    app.run()
+
+# Textual Python Code Reload: https://blog.pecar.me/textual-code-reload
